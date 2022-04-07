144a22865e7a7fccb3fd2b97fa21d9dbee4383e7
Salomón Sánchez A <39140708+devssx@users.noreply.github.com>
2022-04-06 21:32:44 -0700

    + Producto

---
+++ database/migrations/2022_04_07_040842_create_productos_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id(); // cod_prod
+
+            $table->string('nombre', 100);
+            $table->string('codBar', 50);
+            $table->string('familia', 50);
+            $table->string('descripcion', 500);
+            $table->string('especificacion', 500);
+            $table->decimal('precio');
+            $table->unsignedInteger('maximos')->default(0);
+            $table->decimunsignedIntegeral('minimos')->default(0);
+            $table->boolean('stock');
+            $table->tinyInteger('favorito');
+            $table->string('image')->default('');
+
+            $table->timestamps();
+
+            // https://www.codegrepper.com/code-examples/php/laravel+data+types
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
